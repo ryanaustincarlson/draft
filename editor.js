75230391af944c7a94119393b35d2f0cac15a649
Ryan Carlson <ryan.austin.carlson@gmail.com>
2014-06-13 21:30:03 -0400

    round about way to get unordered lists to show up in outline
    - should probably change to a different editor..

--- editor.js
+++ editor.js
@@ -3,10 +3,11 @@ function Editor(divID, document)
 	console.log("Editor created");
 	this.divID = divID;
 	this.rawText = "rawTest";
+	this.document = document;
 
-	this.html = function(document)
+	this.html = function()
 	{	
-		return document.getElementById(this.divID).innerHTML
+		return this.document.getElementById(this.divID).innerHTML
 	}
 }
 
@@ -16,8 +17,9 @@ function WysihtmlEditor(id, divID)
 {
 	this.id = id;
 	this.divID = divID;
+	this.document = document;
 
-	this.makeHtml = function (id, placeholder, autofocus) {
+	this.makeHtml = function (placeholder, autofocus) {
 		var html = "<textarea id=\"" + this.id + "\" ";
 		if (placeholder != null && placeholder.length > 0)
 		{
@@ -31,8 +33,50 @@ function WysihtmlEditor(id, divID)
 		return html;
 	}
 
-	var div = document.getElementById(divID);
-	div.innerHTML += this.makeHtml(this.id, null, false);
+	this.createEditor = function()
+	{
+		var rules = {
+			tags: {
+				"ul": 1,
+				"li": 1
+			}
+		};
+
+		this.editor = new wysihtml5.Editor(this.id, { // id of textarea element
+			toolbar:      this.id + "-toolbar", // id of toolbar element
+  			parserRules:  rules // defined in parser rules set 
+		});
+	}
+	
+	this.initialize = function()
+	{
+		var div = this.document.getElementById(this.divID);
+		div.innerHTML += this.makeHtml(null, false);
+
+		this.createEditor()
+	}
 }
 
 WysihtmlEditor.prototype = new Editor()
+
+function WysihtmlOutlineEditor(id, divID)
+{
+	this.id = id;
+	this.divID = divID;
+	this.document = document;
+
+	this.superInitialize = this.initialize
+	this.initialize = function()
+	{
+		this.superInitialize()
+		// wysihtml5.commands.insertUnorderedList.exec(this.editor.composer, 'insertUnorderedList')
+		// this.editor.composer.commands.exec("insertUnorderedList");
+
+		var ul = "<a data-wysihtml5-command=\"insertUnorderedList\">insert unordered list</a>";
+		var toolbar = this.document.getElementById("wysihtml5-outline-toolbar");
+		toolbar.innerHTML = ul;
+	}
+	// this.composer.commands.exec("insertUnorderedList");
+}
+
+WysihtmlOutlineEditor.prototype = new WysihtmlEditor()
