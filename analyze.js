12ef8b2be7a95b18a292c26b9bf5d79e0b660195
Ryan Carlson <ryan.austin.carlson@gmail.com>
2014-07-13 22:46:22 -0400

    cosine similarity implemented

--- analyze.js
+++ analyze.js
@@ -63,17 +63,110 @@ function Analyzer(editor, outline)
 function Matcher()
 {
 	this.sentenceFeatureMapping = {'a':'b'};
+
+	this.matches = function(s1, s2)
+	{
+		return 1;
+	}
 }
 
 function CosineMatcher()
 {
 	this.matches = function(s1, s2)
 	{
-		console.log(this.sentenceFeatureMapping)
+		var features1 = this.sentenceFeatureMapping[s1];
+		if (!features1)
+		{
+			features1 = this.features(s1);
+			this.sentenceFeatureMapping[s1] = features1
+		}
+
+		var features2 = this.sentenceFeatureMapping[s2];
+		if (!features2)
+		{
+			features2 = this.features(s2)
+			this.sentenceFeatureMapping[s2] = features2
+		}
+
+		// TODO: be smarter about this!
+		var overlapping_words_map = {}
+		for (var key in features1)
+		{
+			if (features1.hasOwnProperty(key))
+			{
+				overlapping_words_map[key] = true;
+			}
+		}
+		for (var key in features2)
+		{
+			if (features2.hasOwnProperty(key))
+			{
+				overlapping_words_map[key] = true;
+			}
+		}
+		var overlapping_words = []
+		for (var key in overlapping_words_map)
+		{
+			if (overlapping_words_map.hasOwnProperty(key))
+			{
+				overlapping_words.push(key)
+			}
+		}
+		console.log(overlapping_words);
+
+		var vector1 = []
+		var vector2 = []
+		for (var i=0; i<overlapping_words.length; i++)
+		{
+			var word = overlapping_words[i];
+			var ftr1 = features1[word];
+			vector1.push(ftr1 ? ftr1 : 0);
+
+			var ftr2 = features2[word];
+			vector2.push(ftr2 ? ftr2 : 0);
+		}
+
+		console.log(features1)
+		console.log(features2)
+
+		if (features1 == features2)
+		{
+			return 1.0;
+		}
+
+		// console.log("vector1: " + vector1);
+		// console.log("vector2: " + vector2);
+
+		var dot_product = 0;
+		var squared_sum1 = 0;
+		var squared_sum2 = 0;
+		for (var i=0; i<vector1.length; i++)
+		{
+			dot_product += (vector1[i] * vector2[i]);
+			squared_sum1 += vector1[i] * vector1[i];
+			squared_sum2 += vector2[i] * vector2[i];
+		}
+		// console.log("dot product: " + dot_product);
+		// console.log("squared sum1: " + squared_sum1);
+		// console.log("squared sum2: " + squared_sum2);
+
+		var sqrt1 = Math.sqrt(squared_sum1);
+		var sqrt2 = Math.sqrt(squared_sum2);
+
+		// console.log("sqrt1: " + sqrt1);
+		// console.log("sqrt2: " + sqrt2);
+
+		// console.log("sqrts eq? " + (sqrt1 == sqrt2))
+		// console.log("sqrt1 * sqrt2: " + (sqrt1 * sqrt2));
+
+		var cosine = dot_product / (sqrt1 * sqrt2);
+
+		return cosine;
 	}
 
 	this.features = function(s)
 	{
+		console.log("getting features")
 		s = s.replace(/[\.,-\/#!?$%\^&\*;:{}=\-_`~()]/g,"");
 		s = s.replace(/\s{2,}/g," ");
 		var words = s.split(" ");
@@ -94,5 +187,13 @@ function CosineMatcher()
 CosineMatcher.prototype = new Matcher();
 
 var matcher = new CosineMatcher();
-var x = matcher.features("hello there. how are you?")
-console.log(x)
+var s1 = "hello hello there. how are you?"
+var s2 = "hi friend. how are you doing?"
+var s3 = "blah argh what okay"
+
+console.log(matcher.matches(s1, s1));
+console.log(matcher.matches(s2, s2));
+console.log(matcher.matches(s1, s2));
+// console.log(matcher.matches(s1, s3));
+// console.log(matcher.matches(s2, s3));
+// matcher.matches(s1, s2)
