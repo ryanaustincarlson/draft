12e326f353b28a55dd9ffdb47291f1959526b6aa
Ryan Carlson <ryan.austin.carlson@gmail.com>
2014-10-08 20:43:20 -0400

    removed test files from gh-pages branch

--- test.js
+++
@@ -1,8 +0,0 @@
-
-var textarea = document.getElementById("text");
-console.log(textarea);
-
-textarea.setAttribute('contenteditable', true);
-
-textarea.innerHTML += "<ul><li><input type=\"checkbox\"></li></ul>";
-// textarea.execCommand("insertUnorderedList", false, null);
